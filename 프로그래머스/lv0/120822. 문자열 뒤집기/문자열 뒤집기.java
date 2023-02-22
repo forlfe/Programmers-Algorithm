88577b4c64f4d193a2832c5ce49134e2b080fa07
Haeyeon Song <forlfe9@gmail.com>
2023-02-22 17:33:49 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,11 +1,12 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
+       String answer= "";
+
+        StringBuilder tmp = new StringBuilder();
+        tmp.append(my_string);
+        tmp.reverse();
         
-      
-        for(int i = my_string.length() -1; i >=0; i-- ){
-            answer = answer + my_string.charAt(i);
-        }
-        return answer;
+        return tmp.toString();
+        
     }
 }
